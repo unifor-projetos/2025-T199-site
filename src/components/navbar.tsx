9cc58ca1a185cc3b58d68d8135d379b505b63511
PedroGaya <pedrohenriquegaya@gmail.com>
2025-05-03 10:16:13 -0300

    add navbar and router

---
+++ src/components/navbar.tsx
@@ -0,0 +1,21 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavBar() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/counter">Counter</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavBar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+import NavBar from "./components/navbar";
+import Counter from "./counter";
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<>Hello world!</>} />
+      <Route path="/counter/:startCount" element={<Counter />} />
+      <Route path="/counter" element={<Counter />} />
+    </Routes>
+  </BrowserRouter>
+);
